a4227ffa7180f1bbb416e43a00d8969a821a5d94
Florent Vejux <flvejux@c1r11s2.42lausanne.ch>
2025-12-13 10:58:46 +0100

    Indexing

--- push_swap.c
+++ push_swap.c
@@ -5,78 +5,72 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: flvejux <flvejux@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/10 08:43:43 by flvejux           #+#    #+#             */
-/*   Updated: 2025/12/10 08:44:04 by flvejux          ###   ########.ch       */
+/*   Created: 2025/12/13 10:50:18 by flvejux           #+#    #+#             */
+/*   Updated: 2025/12/13 10:51:14 by flvejux          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	cleanup_all(char **args, int *numbers, int do_free)
+static int	*get_indexed_numbers(int ac, char **av, int *count, int *do_free)
 {
-	if (numbers)
-		free(numbers);
-	if (do_free && args)
-		ft_free_tab(args);
-}
+	int	*numbers;
+	int	*indexed;
 
-static int	*parse_and_convert(int ac, char **av, int *count, int *do_free)
-{
-	char	**args;
-	int		*numbers;
-
-	args = extract(ac, av, do_free);
-	if (!args)
-		return (NULL);
-	*count = count_args(args);
-	numbers = convert(args, *count);
+	numbers = parse_and_convert(ac, av, count, do_free);
 	if (!numbers)
-	{
-		if (*do_free)
-			ft_free_tab(args);
 		return (NULL);
-	}
-	if (!chk_double(numbers, *count))
-	{
-		cleanup_all(args, numbers, *do_free);
-		return (NULL);
-	}
-	if (*do_free)
-		ft_free_tab(args);
-	return (numbers);
+	indexed = indexing(numbers, count);
+	free(numbers);
+	return (indexed);
 }
 
-static int	get_stack_size(t_stack *stack)
+static int	init_stacks(t_stack **a, t_stack **b, int *indexed, int count)
 {
-	int		size;
-	t_stack	*tmp;
-
-	size = 0;
-	tmp = stack;
-	while (tmp)
-	{
-		size++;
-		tmp = tmp->next;
-	}
-	return (size);
+	*a = create_stack(indexed, count);
+	*b = NULL;
+	free(indexed);
+	if (!(*a))
+		return (0);
+	return (1);
 }
 
-static void	sort_stack(t_stack **a, t_stack **b, int size)
+static void	sort_and_clean(t_stack **a, t_stack **b, int size)
 {
-	if (size == 2)
-		sa(a);
-	else if (size == 3)
-		sort_three(a);
-	else if (size <= 5)
-		sort_five(a, b, size);
-	// else
-	// 	sort_big(a, b, size);
+	sort_stack(a, b, size);
+	free_stack(*a);
+	free_stack(*b);
 }
 
 int	main(int ac, char **av)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
+	int		*indexed;
+	int		count;
+	int		do_free;
+
+	if (!check_entry(ac, av))
+		return (0);
+	indexed = get_indexed_numbers(ac, av, &count, &do_free);
+	if (!indexed)
+		return (write(2, "Error\n", 6), 1);
+	if (!init_stacks(&stack_a, &stack_b, indexed, count))
+		return (write(2, "Error\n", 6), 1);
+	if (is_sorted(stack_a))
+	{
+		free_stack(stack_a);
+		return (0);
+	}
+	sort_and_clean(&stack_a, &stack_b, get_stack_size(stack_a));
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	int		*indexer;
 	int		*numbers;
 	int		count;
 	int		do_free;
@@ -86,6 +80,12 @@ int	main(int ac, char **av)
 	numbers = parse_and_convert(ac, av, &count, &do_free);
 	if (!numbers)
 		return (write(2, "Error\n", 6), 1);
+	indexed = indexing(numbers, count);
+	if (!indexed)
+	{
+		free(numbers);
+		return (write(2, "Error\n", 6), 1);
+	}
 	stack_a = create_stack(numbers, count);
 	stack_b = NULL;
 	free(numbers);
--- quick_sort.c
+++ quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: flox <flox@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2010/12/20 10:15:51 by flvejux           #+#    #+#             */
-/*   Updated: 2025/12/12 12:13:48 by flox             ###   ########.fr       */
+/*   Updated: 2025/12/13 10:46:03 by flox             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,35 @@
 
 void	quick_sort(t_stack **stack)
 {
-	t_stack **a;
-	t_stack **b;
+	t_stack	**a;
+	t_stack	**b;
 
 }
 
-int *indexe_stack(int *tab, int count)
+int	*copy_tab(int *tab, int count)
+{
+	int	i;
+	int	*tmp;
+
+	tmp = malloc(sizeof(int) * count);
+	if (!tmp)
+		return (NULL);
+	i = 0;
+	while (i < count)
+	{
+		tmp[i] = tab[i];
+		i++;
+	}
+	return (tmp);
+}
+
+void	sorting(int *tab, int count)
 {
 	int	i;
 	int	j;
-	
+
 	i = 0;
-	while (j < count - 1)
+	while (i < count - 1)
 	{
 		j = 0;
 		while (j < count - 1 - i)
@@ -36,13 +53,41 @@ int *indexe_stack(int *tab, int count)
 		}
 		i++;
 	}
+}
+
+int	findex(int value, int *sorted, int count)
+{
+	int	i;
+
 	i = 0;
 	while (i < count)
 	{
-		tab[i] = i;
+		if (sorted[i] == value)
+			return (i);
 		i++;
 	}
-	return (tab);
+	return (-1);
 }
 
-void
+int	indexing(int *tab, int count)
+{
+	int	i;
+	int	*index;
+	int	*tmp;
+
+	tmp = copy_tab(tab, count);
+	if (!tmp)
+		return (free (tmp)qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq, NULL);
+	sorting(tmp, count);
+	index = malloc(sizeof(int) * count);
+	if (!index)
+		return (NULL);
+	i = 0;
+	while (i < count)
+	{
+		index[i] = findex(tab[i], tmp, count);
+		i++;
+	}
+	free (tmp);
+	return (index);
+}
